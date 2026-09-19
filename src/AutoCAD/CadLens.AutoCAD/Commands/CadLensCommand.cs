4a2f41100b643ea444e4516ee475f1ce584eab5b
Vildar <khisyametdinovvt@pik.ru>
2026-09-19 13:24:49 +0400

    CadLensCommand

---
+++ src/AutoCAD/CadLens.AutoCAD/Commands/CadLensCommand.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.Runtime;
+using JetBrains.Annotations;
+
+namespace CadLens.AutoCAD;
+
+/// <summary>
+/// Команда для проверки загрузки CAD Lens в AutoCAD.
+/// </summary>
+[UsedImplicitly]
+public sealed class CadLensCommand
+{
+    private const string CommandName = "CADLENS";
+    private const string Greeting = "\nПривет! CAD Lens готов к работе.";
+
+    /// <summary>
+    /// Выводит приветствие в командную строку активного чертежа.
+    /// </summary>
+    [UsedImplicitly]
+    [CommandMethod(CommandName, CommandFlags.NoUndoMarker)]
+    public void Execute()
+    {
+        Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage(Greeting);
+    }
+}
