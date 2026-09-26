faba66ac5419bf840412d6c0fa456a1b4b7890c3
Vildar <khisyametdinovvt@pik.ru>
2026-09-26 18:22:42 +0400

    fix script

--- scripts/New-Bundle.ps1
+++ scripts/New-Bundle.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = 'Stop'
 
 $repository = (Resolve-Path (Join-Path $PSScriptRoot '..')).Path
+$project = Join-Path $repository 'src\AutoCAD\CadLens.AutoCAD\CadLens.AutoCAD.csproj'
 $output = Join-Path $repository 'artifacts\bundle'
 $bundle = Join-Path $output 'CadLens.bundle'
 $contents = Join-Path $bundle 'Contents'
@@ -16,7 +17,7 @@ if (Test-Path -LiteralPath $output) {
 
 New-Item -ItemType Directory -Path $contents -Force | Out-Null
 
-dotnet publish (Join-Path $repository 'src\AutoCAD\CadLens.AutoCAD\CadLens.AutoCAD.csproj') `
+dotnet publish $project `
     --configuration Release `
     --artifacts-path (Join-Path $output 'build') `
     --output $contents `
@@ -29,9 +30,15 @@ if ($LASTEXITCODE -ne 0) {
 
 Get-ChildItem -LiteralPath $contents -Filter '*.xml' | Remove-Item
 
-$manifest = @'
+$version = dotnet msbuild $project -getProperty:Version -property:Configuration=Release
+
+if ($LASTEXITCODE -ne 0 -or [string]::IsNullOrWhiteSpace($version)) {
+    throw 'Could not read the plugin version.'
+}
+
+$manifest = @"
 <?xml version="1.0" encoding="utf-8"?>
-<ApplicationPackage SchemaVersion="1.0" AppVersion="1.0.0.0" ProductCode="{EE37011F-E21D-4DCE-B88C-C54E351D71BF}" Name="CAD Lens" Description="Drawing exploration for AutoCAD">
+<ApplicationPackage SchemaVersion="1.0" AppVersion="$version" ProductCode="{EE37011F-E21D-4DCE-B88C-C54E351D71BF}" Name="CAD Lens" Description="Drawing exploration for AutoCAD">
   <Components>
     <RuntimeRequirements OS="Win64" Platform="AutoCAD|Civil3D" SeriesMin="R25.0" />
     <ComponentEntry AppName="CadLens" ModuleName="./Contents/CadLens.AutoCAD.dll" LoadOnCommandInvocation="True">
@@ -41,7 +48,7 @@ $manifest = @'
     </ComponentEntry>
   </Components>
 </ApplicationPackage>
-'@
+"@
 
 Set-Content -LiteralPath (Join-Path $bundle 'PackageContents.xml') -Value $manifest -Encoding utf8
 Compress-Archive -Path $bundle -DestinationPath $zip
